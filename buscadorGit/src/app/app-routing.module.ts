c49c6a90f06e99c2a2db5adabf533238b52d6721
João Vitor Loureiro <joao.loureiro@paytime.com.br>
2023-04-11 20:26:19 -0300

    feat: show the router outlet and the layout elements

--- buscadorGit/src/app/app-routing.module.ts
+++ buscadorGit/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutClientComponent } from './layouts/layout-client/layout-client.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: LayoutClientComponent,
     loadChildren: () =>
       import('./layouts/layout-client/layout-client.module').then(
         (m) => m.LayoutClientModule
--- buscadorGit/src/app/layouts/layout-client/layout-client-routing.module.ts
+++ buscadorGit/src/app/layouts/layout-client/layout-client-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from 'src/app/pages/home/home.component';
 
-const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent },
-];
+const routes: Routes = [{ path: 'home', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
