826b3b34d8886697f294bf090ce3fa061c4e8c8b
João Vitor Loureiro <joao.loureiro@paytime.com.br>
2023-04-11 22:22:20 -0300

    feat: added background img

--- buscadorGit/src/app/layouts/layout-client/layout-client.module.ts
+++ buscadorGit/src/app/layouts/layout-client/layout-client.module.ts
@@ -5,9 +5,10 @@ import { LayoutClientRoutingModule } from './layout-client-routing.module';
 import { HomeComponent } from 'src/app/pages/home/home.component';
 import { LayoutClientComponent } from './layout-client.component';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { SearchPageComponent } from 'src/app/pages/search-page/search-page.component';
 
 @NgModule({
-  declarations: [HomeComponent, LayoutClientComponent],
+  declarations: [HomeComponent, LayoutClientComponent, SearchPageComponent],
   imports: [CommonModule, LayoutClientRoutingModule, ComponentsModule],
 })
 export class LayoutClientModule {}
