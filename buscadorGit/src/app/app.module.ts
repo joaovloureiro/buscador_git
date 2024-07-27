a8f0fb4206e62a43b05f0f680f35ebdf8f9140ac
João Vitor Loureiro <joao.loureiro@paytime.com.br>
2024-07-27 12:00:51 -0300

    fix: resolved locale in date pipe

--- buscadorGit/src/app/app.module.ts
+++ buscadorGit/src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { DatePipe, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }, DatePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
